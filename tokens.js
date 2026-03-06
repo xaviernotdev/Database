e7529bfae3ed030bc2543779cd251f487a02b874
luxxyoner <amdedek289@gmail.com>
2026-03-07 05:14:55 +0800

    Update tokens

--- tokens.js
+++ tokens.js
@@ -1,6 +1,5 @@
 {
   "tokens": [
-    "8661159203:AAFWGXiqbrD4TpsmXca9mI_Yvv1cgQaYSnY",
-    "8730554608:AAHZGq2RBXSBa0jC7dDW4uBwKrej2CUl5Ms"
+    "8661159203:AAFWGXiqbrD4TpsmXca9mI_Yvv1cgQaYSnY"
   ]
 }
