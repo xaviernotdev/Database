fc057815f3fc4a91d6ab06a39ef5e9a7b275a52d
luxxyoner <amdedek289@gmail.com>
2026-03-07 04:34:08 +0800

    Update tokens

--- tokens.js
+++ tokens.js
@@ -1,4 +1,6 @@
 {
-  "tokens":["8661159203:AAFWGXiqbrD4TpsmXca9mI_Yvv1cgQaYSnY"
-]
+  "tokens": [
+    "8661159203:AAFWGXiqbrD4TpsmXca9mI_Yvv1cgQaYSnY",
+    "8730554608:AAHZGq2RBXSBa0jC7dDW4uBwKrej2CUl5Ms"
+  ]
 }
