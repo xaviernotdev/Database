982eb293516533030316af68223326a912e77142
Xavier-NotDev <amdedek289@gmail.com>
2026-03-12 03:30:58 +0800

    Update tokens

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8661159203:AAFWGXiqbrD4TpsmXca9mI_Yvv1cgQaYSnY",
-    "8701857799:AAHBxE_0NPYoaLJQaXDXPkNNmqserbipiy0"
+    "8701857799:AAHBxE_0NPYoaLJQaXDXPkNNmqserbipiy0",
+    "8192706661:AAGH_mZskttMpT-YIUWWX3-I-L8O3QVj0RE"
   ]
 }
