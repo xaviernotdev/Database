60675410c0a3c4e1db83741619c5fd43f43ec079
Xavier-NotDev <amdedek289@gmail.com>
2026-05-09 01:45:35 +0800

    Update tokens

--- tokens.js
+++ tokens.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8649910601:AAFC-OMYiX_XBpHX_ln-3OVfqyYYPopH0tA"
+    "8649910601:AAFC-OMYiX_XBpHX_ln-3OVfqyYYPopH0tA",
+    "8747573751:AAFLcxj4p-oz6OCRdL-iBSVGcMCcfSa9P0k"
   ]
 }
