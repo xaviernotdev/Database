238e520c7f694f6556c03d6a0cee872a8a1262a3
Xavier-NotDev <amdedek289@gmail.com>
2026-06-05 11:38:40 +0800

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,6 +1,6 @@
 {
   "tokens": [
-    "8875568653:AAEAjNDBohZVBGtAwqYN-eBQgOGaXv1hqHg",
+    "8885681497:AAGHjxRq845RBVWyuG0CiGbivpvIGs7udnE",
     "8288842631:AAHoOR2lVV9BpAcUcBnehls9Mm26fwLspiQ"
   ]
 }
