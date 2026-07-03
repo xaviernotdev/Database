76930b2c77cb568ab4f62f83afbfdeef2a75d2f4
Xavier-NotDev <amdedek289@gmail.com>
2026-07-04 03:56:26 +0800

    delete

--- tokens.js
+++ tokens.js
@@ -1,7 +1,6 @@
 {
   "tokens": [
     "8979335581:AAEl-jNBGDiOmIRuENlq0uL-a_hzZE6wqqA",
-    "8288842631:AAHoOR2lVV9BpAcUcBnehls9Mm26fwLspiQ",
-    "Pppp"
+    "8288842631:AAHoOR2lVV9BpAcUcBnehls9Mm26fwLspiQ"
   ]
 }
