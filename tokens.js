d12dc80c6c0c23d0bee9403e9bbca71f3924f66d
Xavier-NotDev <amdedek289@gmail.com>
2026-07-04 05:04:56 +0800

    add

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8979335581:AAEl-jNBGDiOmIRuENlq0uL-a_hzZE6wqqA",
-    "8288842631:AAHoOR2lVV9BpAcUcBnehls9Mm26fwLspiQ"
+    "8288842631:AAHoOR2lVV9BpAcUcBnehls9Mm26fwLspiQ",
+    "Beginer"
   ]
 }
