f73029fab59002bc63d751b148c619a19cd05501
Xavier-NotDev <amdedek289@gmail.com>
2026-07-08 13:18:44 +0800

    update tokens 2026-07-08T05:18:44.437Z

--- tokens.js
+++ tokens.js
@@ -9,12 +9,6 @@
       "token": "8581294586:AAG0ba4Is8iyr8T6U5mrWgGYXWiTqtUgPnw",
       "expiresAt": null,
       "addedBy": null
-    },
-    {
-      "token": "1",
-      "expiresAt": 1783487741828,
-      "addedBy": "1238571022",
-      "reminded": true
     }
   ]
 }
