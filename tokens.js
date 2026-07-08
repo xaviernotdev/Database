52af8a4f5c91cf25566706397bea077c95c3f3c2
Xavier-NotDev <amdedek289@gmail.com>
2026-07-08 14:01:47 +0800

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,26 +1,5 @@
 {
   "tokens": [
-    {
-      "token": "8979335581:AAEl-jNBGDiOmIRuENlq0uL-a_hzZE6wqqA",
-      "expiresAt": null,
-      "addedBy": null
-    },
-    {
-      "token": "8581294586:AAG0ba4Is8iyr8T6U5mrWgGYXWiTqtUgPnw",
-      "expiresAt": null,
-      "addedBy": null
-    },
-    {
-      "token": "7841570114:AAEmWDchHHLM94LVTLA0fblY37xSu3KYBGQ",
-      "expiresAt": null,
-      "addedBy": "1238571022",
-      "reminded": false
-    },
-    {
-      "token": "8640633138:AAHpVVRWDxFcMBDt178AB80th7n6YaI5dIA",
-      "expiresAt": null,
-      "addedBy": "1238571022",
-      "reminded": false
-    }
+    "8640633138:AAHpVVRWDxFcMBDt178AB80th7n6YaI5dIA"
   ]
 }
