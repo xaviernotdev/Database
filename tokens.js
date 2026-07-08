cad259a38f96dde5070a490b9220a4f30ce2217b
Xavier-NotDev <amdedek289@gmail.com>
2026-07-08 13:37:57 +0800

    update tokens 2026-07-08T05:37:57.669Z

--- tokens.js
+++ tokens.js
@@ -9,6 +9,12 @@
       "token": "8581294586:AAG0ba4Is8iyr8T6U5mrWgGYXWiTqtUgPnw",
       "expiresAt": null,
       "addedBy": null
+    },
+    {
+      "token": "7841570114:AAEmWDchHHLM94LVTLA0fblY37xSu3KYBGQ",
+      "expiresAt": null,
+      "addedBy": "1238571022",
+      "reminded": false
     }
   ]
 }
