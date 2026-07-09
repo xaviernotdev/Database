87616fd7d29f77f378ff838a7dca703ec17f41d8
Xavier-NotDev <amdedek289@gmail.com>
2026-07-09 16:15:43 +0800

    Update token list

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8640633138:AAHpVVRWDxFcMBDt178AB80th7n6YaI5dIA",
-    "8585907558:AAFsHnDak_WccOQkfiH3APkpRsvuI1cSCfE"
+    "8585907558:AAFsHnDak_WccOQkfiH3APkpRsvuI1cSCfE",
+    "8403915311:AAF3nwpqe2gKNM0jldEGQ17A9WOTv_qafXg"
   ]
 }
